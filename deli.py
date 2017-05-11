0925c838d40c35267c517c7dfe88baa13ab0482e
tdoaty <tdoaty@google.com>
2017-05-11 13:31:21 -0400

    updated

--- deli.py
+++ deli.py
@@ -1,26 +1,16 @@
 katz_deli = []
 def line(katz_deli):
-    deli_list_string = "The line is currently: "
+    deli_list_string = "The line is currently:"
     for i in range(0,len(katz_deli)):
-        deli_list_string += stri(i+1) + ". " list_of_customers[i] + " "
+        deli_list_string += " " + str(i+1) + ". " list_of_customers[i]
+    if len(katz_deli) == 0
+        return "The line is currently empty."
     return deli_list_string
 
-def take_a_number(katz_deli, name):
-    katz_deli.append(name)
-        print "Welcome, " + name + ". You are number" + katz_deli + " in line."
-    return take_a_number
+def take_a_number(katz_deli, new_customer):
+    katz_deli.append(new_custoemr)
+    return "Welcome, " + new_customer+ ". You are number " + str(len(katz_deli)) + "in line."
 
 def now_serving(katz_deli):
-    katz_deli.pop(0)
-        print "Currently serving" + katz_deli[0]
-    if len(katz_deli) == 0
-        print "There is nobody waiting to be served!"
-    return now_serving
-
-
-
-
-def take_a_number(katz_deli, new_customer):
-
-
-def now_serving():
+    current_customer = katz_deli.pop(0)
+    return "Currently serving " + current_customer + "."
